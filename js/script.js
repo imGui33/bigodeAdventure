91af4de50daed7f533bd1fa6113945f08ace54fa
imGuii <69533374+imGui33@users.noreply.github.com>
2023-03-25 18:45:13 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const mario = document.querySelector(".mario");
+const pipe = document.querySelector(".pipe");
+let death = false;
+
+const jump = () => {
+  mario.classList.add("jump");
+
+  setTimeout(() => {
+    mario.classList.remove("jump");
+  }, 500);
+};
+
+const loop = setInterval(() => {
+  const pipePostion = pipe.offsetLeft;
+  const marioPostion = +window.getComputedStyle(mario).bottom.replace("px", "");
+
+  if (pipePostion <= 120 && pipePostion > 0 && marioPostion < 80) {
+    pipe.style.animation = "none";
+    mario.style.animation = "none";
+
+    mario.src = "../images/morreu.png";
+    pipe.style.left = `${pipePostion}px`;
+
+    mario.style.bottom = `${marioPostion}px`;
+
+    const audio = new Audio("../audios/morreu.ogg");
+    audio.play();
+
+    clearInterval(loop);
+  }
+}, 10);
+
+document.addEventListener("keydown", jump);
